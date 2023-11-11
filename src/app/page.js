a65e30a09d47ab051b0241e111a398937dc2b925
niph <muhammadhnf97@gmail.com>
2023-11-11 15:18:58 +0700

    feature : random anime for your weekend

--- src/app/page.js
+++ src/app/page.js
@@ -18,6 +18,7 @@ export default function Home() {
   const [topAnime, setTopAnime] = useState([])
   const [animeReview, setAnimeReview] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [topAnimePagination, setTopAnimePagination] = useState({})
 
   useEffect(()=>{
     getApi('/seasons/now?limit=5&&filter=tv')
@@ -80,6 +81,7 @@ export default function Home() {
               }
             })
           })
+          setTopAnimePagination(data.data.pagination)
         })
         .catch(setTopAnime([]))
       }, 1000)
@@ -107,7 +109,6 @@ export default function Home() {
               }
             }).slice(0,5)
           })
-          setIsLoading(prev=>!prev)
         })
         .catch(setAnimeReview([]))
       }, 1000)
@@ -115,76 +116,121 @@ export default function Home() {
       return () => clearTimeout(reviewAnime)
     }
   }, [topAnime])
+
+  const [randomAnimes, setRandomAnimes] = useState([])
+
+  useEffect(()=>{
+    if (animeReview.length > 0) {
+      const random = setTimeout(()=>{
+        const randomNumber = Math.round(Math.random() * 10)
+        getApi(`/top/anime?page=${randomNumber}&swf=true`)
+        .then(data=>{
+          setRandomAnimes(()=>{
+            return data.data.data.filter(value=>value.type === 'TV').map(values=>{
+              return {
+                mal_id: values.mal_id,
+                image_url: values.images.webp.image_url,
+                rating: values.rating,
+                title: values.title,
+                score: values.score,
+                popularity: values.popularity,
+                section: 'top'
+              }
+            }).slice(0,5)
+          })
+          setIsLoading(false)
+        })
+        .catch(setRandomAnimes([]))
+      }, 1000)
+
+      return () => clearTimeout(random)
+    }
+  }, [animeReview])
   return (
     <>
     {
       isLoading ? 
       <Loading />
       :
-      <main className="max-w-7xl mx-auto w-full bg-white flex flex-col gap-3 md:flex-row">
-        <section className="w-full p-3 md:w-3/5">
-          <section className="w-full">
-            <div className="flex justify-between items-center py-2">
-              <h3 className="text-xl font-semibold">Current Seasons's Animes</h3>
-              <Link href={'/'}><p className="text-sm text-blue-400">View more</p></Link>
-            </div>
-            <div className="w-full space-y-3 overflow-x-auto">
-              <div className="w-fit mx-auto flex gap-5">
-              {
-                currentSeasonAnime.map(values=>(
-                    <AnimeSeasonsCard
-                      key={values.mal_id}
-                      values={values} 
-                    />
-                  ))
-                }
+      <main className="max-w-7xl mx-auto w-full bg-white">
+        <section className="w-full flex flex-col gap-3 md:flex-row">
+          <section className="w-full p-3 md:w-3/5">
+            <section className="w-full">
+              <div className="flex justify-between items-center py-2">
+                <h3 className="text-xl font-semibold">Current Seasons's Animes</h3>
+                <Link href={'/'}><p className="text-sm text-blue-400">View more</p></Link>
               </div>
-            </div>
+              <div className="w-full space-y-3 overflow-x-auto">
+                <div className="w-fit mx-auto flex gap-5">
+                {
+                  currentSeasonAnime.map(values=>(
+                      <AnimeSeasonsCard
+                        key={values.mal_id}
+                        values={values} 
+                      />
+                    ))
+                  }
+                </div>
+              </div>
+            </section>
+            <section className="w-full">
+              <div className="flex justify-between items-center py-2">
+                <h3 className="text-xl font-semibold">Last Anime Review </h3>
+              </div>
+              <div className="w-full space-y-3 overflow-x-auto">
+                <div className="w-full mx-auto flex flex-col gap-5">
+                {
+                  animeReview.map(values=>(
+                      <AnimeReviews
+                        key={values.mal_id}
+                        values={values}/>
+                    ))
+                  }
+                </div>
+              </div>
+            </section>
           </section>
-          <section className="w-full">
-            <div className="flex justify-between items-center py-2">
-              <h3 className="text-xl font-semibold">Last Anime Review </h3>
-            </div>
-            <div className="w-full space-y-3 overflow-x-auto">
-              <div className="w-full mx-auto flex flex-col gap-5">
-              {
-                animeReview.map(values=>(
-                    <AnimeReviews
-                      key={values.mal_id}
-                      values={values}/>
-                  ))
-                }
+          <section className="w-full bg-white md:w-2/5">
+            <section className="w-full bg-primary p-3">
+              <div className="flex justify-between items-center py-2">
+                <h3 className="text-xl font-semibold text-white">Upcoming Animes</h3>
+                <Link href={'/upcoming'}><p className="text-sm text-blue-200">View more</p></Link>
               </div>
-            </div>
+              {
+                upcomingAnime.map(values=>(
+                  <AnimeCards
+                  key={values.mal_id}
+                  values={values} />
+                ))
+              }
+            </section>
+            <section className="w-full bg-slate-600 p-3">
+              <div className="flex justify-between items-center py-2">
+                <h3 className="text-xl font-semibold text-white">Top Animes</h3>
+                <Link href={'/topanime'}><p className="text-sm text-blue-200">View more</p></Link>
+              </div>
+              {
+                topAnime.map(values=>(
+                  <AnimeCards
+                  key={values.mal_id}
+                  values={values} />
+                ))
+              }
+            </section>
           </section>
         </section>
-        <section className="w-full bg-white md:w-2/5">
-          <section className="w-full bg-primary p-3">
-            <div className="flex justify-between items-center py-2">
-              <h3 className="text-xl font-semibold text-white">Upcoming Animes</h3>
-              <Link href={'/upcoming'}><p className="text-sm text-blue-200">View more</p></Link>
-            </div>
-            {
-              upcomingAnime.map(values=>(
-                <AnimeCards
-                key={values.mal_id}
-                values={values} />
+        <section className="bg-black text-white p-3 space-y-3">
+          <h3 className="text-xl font-semibold text-white">Random animes for your weekend !</h3>
+          <div className="grid grid-cols-2 gap-3 mx-auto md:grid-cols-5">
+              {
+              randomAnimes.map(values=>(
+                <Link href={`/anime/${values.mal_id}`} key={values.mal_id}>
+                  <AnimeSeasonsCard
+                  values={values} />
+                </Link>
               ))
-            }
-          </section>
-          <section className="w-full bg-slate-600 p-3">
-            <div className="flex justify-between items-center py-2">
-              <h3 className="text-xl font-semibold text-white">Top Animes</h3>
-              <Link href={'/topanime'}><p className="text-sm text-blue-200">View more</p></Link>
-            </div>
-            {
-              topAnime.map(values=>(
-                <AnimeCards
-                key={values.mal_id}
-                values={values} />
-              ))
-            }
-          </section>
+              }
+          </div>
         </section>
       </main>
     }
